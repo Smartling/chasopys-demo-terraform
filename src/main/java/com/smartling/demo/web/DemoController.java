fb8e5f5fc36199e2dbb700efec74b7b505b6cc39
vlinetskyi <vlinetskyi@smartling.com>
2017-03-31 10:47:12 +0300

    commit 2

--- src/main/java/com/smartling/demo/web/DemoController.java
+++ src/main/java/com/smartling/demo/web/DemoController.java
@@ -11,10 +11,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Controller
 public class DemoController
 {
-    @RequestMapping(value = "/chasopys-api/v1/greeting", method = GET)
+    @RequestMapping(value = "/chasopys-api/v1/greeting/{adjective}", method = GET)
     @ResponseBody
-    Greeting getGreeting()
+    Greeting getGreeting(@PathVariable(name = "adjective") String adjective)
     {
-        return new Greeting("Hi, Chasopys!");
+        return new Greeting("Hi, " + adjective + " Chasopys!");
     }
 }
